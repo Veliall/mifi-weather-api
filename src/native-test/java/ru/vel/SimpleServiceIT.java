0bfc79bb10096f455a456860d567451939b1fbbb
Христюк Игорь Анатольевич <IKhristyuk@alfabank.ru>
2024-10-27 12:02:48 +0300

    deleted unnecessary files

--- src/native-test/java/ru/vel/SimpleServiceIT.java
+++
@@ -1,8 +0,0 @@
-package ru.vel;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class SimpleServiceIT extends SimpleServiceTest {
-    // Execute the same tests but in packaged mode.
-}
